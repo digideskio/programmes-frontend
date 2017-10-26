736ad1c1c20e9f845e456f48dc2a0bcc8d33da03
Matt Greenham <matt.greenham@bbc.co.uk>
2017-10-26 16:27:47 +0100

    Here goes nothing

--- src/Controller/FacebookController.php
+++ src/Controller/FacebookController.php
@@ -3,9 +3,11 @@ declare(strict_types = 1);
 
 namespace App\Controller;
 
+use BBC\ProgrammesPagesService\Domain\Entity\Programme;
 use GuzzleHttp\Client;
 use stdClass;
 use Symfony\Component\HttpFoundation\Request;
+use BBC\ProgrammesPagesService\Service\ProgrammesService;
 
 class FacebookController extends BaseController
 {
@@ -40,7 +42,7 @@ class FacebookController extends BaseController
         return $this->response();
     }
 
-    public function edit(Request $request)
+    public function edit(Request $request, ProgrammesService $programmesService)
     {
         $body = json_decode($request->getContent());
         // Checks this is an event from a page subscription
@@ -53,7 +55,7 @@ class FacebookController extends BaseController
                 $webhookEvent = $entry->messaging[0];
                 $senderPsid = $webhookEvent->sender->id;
                 if (isset($webhookEvent->message) && $webhookEvent->message) {
-                    $this->handleMessage($senderPsid, $webhookEvent->message);
+                    $this->handleMessage($senderPsid, $webhookEvent->message, $programmesService);
                 } elseif (isset($webhookEvent->postback) && $webhookEvent->postback) {
                     $this->handlePostback($senderPsid, $webhookEvent->postback);
                 } else {
@@ -85,11 +87,50 @@ class FacebookController extends BaseController
     }
 
 
-    private function handleMessage(string $senderPsid, stdClass $message)
+    private function handleMessage(string $senderPsid, stdClass $message, ProgrammesService $programmesService)
     {
         if ($message->text === 'Hey') {
-            $response = ['text' => 'Hey. Would you like to know when something is next on?'];
-        } else {
+            $response = ['text' => 'Hey. Welcome to the Programmes chatbot.'];
+        }
+        elseif (strpos($message->text, 'Next on: ') !== false) {
+            $userProgramme = str_replace('Next on: ', '', $message->text);
+
+            //do some lookup
+
+            if ($programmesService->countByKeywords($userProgramme) === 0) {
+                $response = ['text' => 'Sorry, we couldn\'t find any programmes.'];
+            }
+            elseif ($programmesService->countByKeywords($userProgramme) === 1) {
+                $results = $programmesService->searchByKeywords($userProgramme);
+
+                /** @var Programme $programme */
+                $programme = $results[0];
+                $response = ['text' => 'We found one programme: '.$programme->getTitle().'. It is next on xxxxx'];
+            }
+            else {
+                $results = $programmesService->searchByKeywords($userProgramme);
+
+                $responseButtons = [];
+
+                foreach ($results as $result) {
+                    $pid = $result->getPid();
+                    $title = $result->getTitle();
+                    array_push($responseButtons, ['type' => 'postback', 'title' => $title, 'payload' => $pid]);
+                }
+
+                $response = [
+                    'attachment' => [
+                        'type' => 'template',
+                        'payload' => [
+                            'template_type' => 'button',
+                            'text' => 'We found these Programmes. Which one did you mean?',
+                            'buttons' => $responseButtons
+                        ],
+                    ]
+                ];
+            }
+        }
+        elseif ($message->text === 'help') {
             $response = [
                 'attachment' => [
                     'type' => 'template',
@@ -114,9 +155,12 @@ class FacebookController extends BaseController
                     ],
                 ]
             ];
+        } else {
+            $response = ['text' => 'Type help for some options.'];
         }
+
+
         $this->send($senderPsid, $response);
-        $this->send($senderPsid, ['text' => 'Second message']);
     }
 
     private function handlePostback(string $senderPsid, $postback)
