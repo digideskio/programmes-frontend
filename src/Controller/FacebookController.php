d7c60ad12950a80b81b1b0540c1d92f8dcccb80b
Michael Gwynne <michael.gwynne@bbc.co.uk>
2017-10-26 14:17:41 +0100

    MANDMHACKDAY Initial Hack Commit

---
+++ src/Controller/FacebookController.php
@@ -0,0 +1,99 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\Request;
+
+class FacebookController extends BaseController
+{
+    const ACCESS_TOKEN = '';
+
+
+    public function show(Request $request)
+    {
+        // Your verify token. Should be a random string.
+        $verifyToken = '';
+
+        // Parse the query params
+        $mode = $request->query->get('hub_mode');
+        $token = $request->query->get('hub_verify_token');
+        $challenge = $request->query->get('hub_challenge');
+
+        // Checks if a token and mode is in the query string of the request
+        if ($mode && $token) {
+
+            // Checks the mode and token sent is correct
+            if ($mode === 'subscribe' && $token === $verifyToken) {
+
+                // Responds with the challenge token from the request
+                $this->response()->setContent($challenge); //200OK
+
+            } else {
+                // Responds with '403 Forbidden' if verify tokens do not match
+                $this->response()->setStatusCode(403);
+            }
+        }
+
+        return $this->response();
+    }
+
+    public function edit(Request $request)
+    {
+        $body = json_decode($request->getContent());
+        // Checks this is an event from a page subscription
+        if ($body->object === 'page') {
+
+            // Iterates over each entry - there may be multiple if batched
+            foreach ($body->entry as $entry) {
+                // Gets the message. entry.messaging is an array, but
+                // will only ever contain one message, so we get index 0
+                $webhookEvent = $entry->messaging[0];
+                if ($webhookEvent->message && $webhookEvent->message->text) {
+//                    $response = ['text' => 'You sent the message: ' . $webhookEvent->message->text . '. Now send me an image!'];
+                    $response = [
+                            'attachment' => [
+                                'type' => 'template',
+                                'payload' => [
+                                    'template_type' => 'button',
+                                    'text' => 'What would you like to know about?',
+                                    'buttons' => [
+                                        [
+                                            'type' => 'postback',
+                                            'title' => 'Eastenders',
+                                            'payload' => 'b006m86d',
+                                        ],[
+                                            'type' => 'postback',
+                                            'title' => 'Dr Who',
+                                            'payload' => 'b006q2x0',
+                                        ],[
+                                            'type' => 'postback',
+                                            'title' => 'torchwood',
+                                            'payload' => 'b006m8ln',
+                                        ],
+                                    ]
+                                ],
+                            ]
+                    ];
+                    $this->send($webhookEvent->sender->id, $response);
+                }
+            }
+
+            // Returns a '200 OK' response to all requests
+            $this->response()->setContent('EVENT_RECEIVED'); //200OK
+        } else {
+            // Returns a '404 Not Found' if event is not from a page subscription
+            $this->response()->setStatusCode(404);
+        }
+
+        return $this->response();
+    }
+
+    private function send($id, $response)
+    {
+        $message = ['recipient' => ['id' => $id], 'message' => $response];
+        $client = new Client();
+        $client->request('POST', 'https://graph.facebook.com/v2.6/me/messages?access_token=' . self::ACCESS_TOKEN, ['json' => $message]);
+    }
+}
