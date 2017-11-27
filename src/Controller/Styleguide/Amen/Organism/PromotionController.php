c9904c470e8eefa26e9b0cb37b35c3b4bc929593
Francisco Albusac <francisco.albusac@bbc.co.uk>
2017-11-27 15:40:46 +0000

    changed also folder structure to mimic folder of dsamen

--- src/Controller/Styleguide/Amen/Organism/PromotionController.php
+++ src/Controller/Styleguide/Amen/Organism/PromotionController.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types = 1);
-namespace App\Controller\Styleguide\Amen;
+namespace App\Controller\Styleguide\Amen\Organism;
 
 use App\Builders\EpisodeBuilder;
 use App\Builders\ImageBuilder;
